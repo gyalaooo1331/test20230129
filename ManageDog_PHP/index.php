72d16d52a9ca21c8d0570dea606c34261e83baa5
gyalaooo1331 <123883630+gyalaooo1331@users.noreply.github.com>
2023-01-29 23:52:25 +0900

    Add files via upload

--- ManageDog_PHP/index.php
+++ ManageDog_PHP/index.php
@@ -5,7 +5,7 @@
   <title>”キョロ”の健康管理表</title>
 </head>
 <body>
-  <h1>一覧</h1>
+  <h1>”キョロ”の健康管理表</h1>
   <a href="form.html">記録作成</a>
 <?php
 $user = 'hmki';
@@ -16,6 +16,10 @@ $dbh->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 $sql = 'SELECT * FROM dogmng';
 $stmt = $dbh->query($sql);
 $result = $stmt->fetchAll(PDO::FETCH_ASSOC);
+//$amountSumSql = 'SELECT SUM(amount) AS sum FROM dogmng GROUP BY date';
+//$stmt_amount = $dbh->query($amountSumSql);
+//$sumAmount = $stmt_amount->fetch(PDO::FETCH_ASSOC);
+//var_dump([$sumAmount]);
 echo '<table border=1>' . PHP_EOL;
 echo '<tr>' . PHP_EOL;
 echo '<th>日付</th><th>時間</th><th>量</th>' . PHP_EOL;
@@ -25,6 +29,7 @@ foreach ($result as $row) {
   echo '<td>' . htmlspecialchars($row['date'], ENT_QUOTES) . '</td>' . PHP_EOL;
   echo '<td>' . htmlspecialchars($row['time'], ENT_QUOTES)  . '</td>' . PHP_EOL;
   echo '<td>' . htmlspecialchars($row['amount'], ENT_QUOTES)  . '</td>' . PHP_EOL;
+  //echo '<td>' . htmlspecialchars($sumAmount['sum'], ENT_QUOTES)  . '</td>' . PHP_EOL;
   echo '<td>' . PHP_EOL;
   echo '<a href="detail.php?id=' . htmlspecialchars($row['id'], ENT_QUOTES) . '">詳細</a>' . PHP_EOL;
   echo '<a href="edit.php?id=' . htmlspecialchars($row['id'], ENT_QUOTES) . '">変更</a>' . PHP_EOL;
